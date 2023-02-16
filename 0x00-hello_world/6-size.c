0eadc2b3c3f7af17a53d52f4ccf50085b83fd034
Yishak <yishakyared12@gmail.com>
2023-02-16 09:38:17 -0800

    updated the answer

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+ * main - main block
+ * Return: 0
+ */
 int main(void)
 {
 	char charType;
@@ -6,6 +10,7 @@ int main(void)
 	float floatType;
 	long longType;
 	long long longlongType;
+	
 	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
 	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(longType));
