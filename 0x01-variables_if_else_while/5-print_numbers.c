465010b8e188d142b69813f8ae86e4874b3231f7
Yishak <yishakyared12@gmail.com>
2023-02-17 09:36:20 -0800

    added answer

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ *
+ * Return: always 0 (sucess)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 0; ch <= 9; ch++)
+	{
+		printf("%d", ch);
+	}
+	return (0);
+}
