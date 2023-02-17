d72e938d7722a43889eeab086986dcbcb70d0af2
Yishak <yishakyared12@gmail.com>
2023-02-17 11:09:54 -0800

    updated answer

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ *
+ * Return: always 0 (sucess)
+ */
+int main(void)
+{
+	char ch;
+	int numbr;
+	
+	for (numbr = 0; numbr <= 9; numbr++)
+	{
+		putchar('0' + numbr);
+	}
+	
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
