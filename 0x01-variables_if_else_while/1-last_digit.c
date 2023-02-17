8936a9c5bf5e1348c47756f6229310d5c4518a52
Yishak <yishakyared12@gmail.com>
2023-02-17 08:46:30 -0800

    added answer

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,7 +21,7 @@ int main(void)
 	}
 	else if (n % 10 == 0)
 	{
-	       printf("Last digit of %d is %d and is 0\n", n, lastdigit);
+		printf("Last digit of %d is %d and is 0\n", n, lastdigit);
 	}
 	else
 	{
