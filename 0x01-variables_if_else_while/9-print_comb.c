7fb8ad0d7b5bf69e30cc027b36018c74419d4a3b
Yishak <yishakyared12@gmail.com>
2023-02-17 11:16:51 -0800

    updated answer

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ *
+ * Return: always 0 (sucess)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 0; ch <= 9; ch++)
+	{
+		putchar('0' + ch);
+		putchar(',')
+	}
+	putchar('\n');
+	return (0);
+}
