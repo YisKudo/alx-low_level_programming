b9c9a1cbd4a7479bda62babaf23088dc6226bed7
Yishak <yishakyared12@gmail.com>
2023-02-17 12:55:38 -0800

    fixed

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ *
+ * Return: always 0 (sucess)
+ */
+int main(void)
+{
+	int ch = 0;
+	int hc = 0;
+	int zc = 1;
+	while (2 > 1)
+	{
+		hc++;
+		if (ch < hc)
+		{
+			putchar('0' + ch);
+			putchar('0' + zc)
+			putchar('0' + hc);
+			if (ch != 8)
+			{
+				putchar(',');
+				putchar(32);
+			}
+		if (hc == 9)
+		{
+			zc++;
+			hc = zc - 1;
+			if (zc == 8)
+			{
+				hc++;
+			}
+		}
+		}
+		if (ch == 9 && hc == 8)
+		{
+			break;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
