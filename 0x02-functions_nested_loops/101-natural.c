025d859436c946f65d617aa167e629e57ddfbdfc
Yishak <yishakyared12@gmail.com>
2023-02-21 11:29:36 -0800

    ansr

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void){
+    int i=0, j = 0;
+
+    while(i != 1024){
+        if(i%3 || i%5){
+            j = i + j;
+        }
+        i++;
+    }
+    printf("%d\n", j);
+}
