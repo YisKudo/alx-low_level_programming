f7201ed9f3d6ec270b6be7255ac65d5bb25a2928
Yishak <yishakyared12@gmail.com>
2023-02-21 11:48:35 -0800

    ansr

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,17 +2,16 @@
 /**
  * main - checks for lowercse
  *
- * 
  *
  * Return: 1 if lowercase or 0 if upper
  */
 
 int main(void)
 {
-	int i=1024, j = 0;
-	while(i > 0)
+	int i = 1024, j = 0;
+	while (i > 0)
 	{
-		if( (i % 3 == 0) || (i % 5 == 0))
+		if ((i % 3 == 0) || (i % 5 == 0))
 		{
 			j = i + j;
 		}
