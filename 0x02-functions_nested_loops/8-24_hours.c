4265e157d6e39f0cfecd83049e532962a8d493a6
Yishak <yishakyared12@gmail.com>
2023-02-21 09:47:06 -0800

    added answer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * jack_bauer - checks for lowercse
+ *
+ * @c: the input
+ *
+ * Return: 1 if lowercase or 0 if upper
+ */
+
+void jack_bauer(void)
+{
+	for (int i = 0; i <= 23; i++)
+	{
+		_putchar(i);
+		_putchar(':');
+		for(int j = 0; j <= 59; j++)
+		{
+			_putchar(j);
+		}
+	}
