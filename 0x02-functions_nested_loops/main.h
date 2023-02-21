4a07f6a7b2e37c3a108db47f7b3a8aae02300c90
Yishak <yishakyared12@gmail.com>
2023-02-21 07:27:20 -0800

    added mainh

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -12,11 +12,4 @@ int _putchar(char c)
 	return (write(1, &c, 1));
 }
 
-void print_alphabet(void)
-{
-	for (int ch = 'a'; ch <= 'z'; ch++)
-	{
-		_putchar(ch);
-	}
-	_putchar("\n");
-}
+void print_alphabet(void);
