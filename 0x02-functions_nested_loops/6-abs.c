56083045649b9e08a0ead54e0a1a47072e5f87cf
Yishak <yishakyared12@gmail.com>
2023-02-21 09:22:14 -0800

    added answer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - checks for lowercse
+ *
+ * @c: the input
+ *
+ * Return: 1 if lowercase or 0 if upper
+ */
+
+int _abs(int c)
+{
+	if (c < 0)
+	{
+		c = (-1) * c;
+	}
+	return (c);
+}
