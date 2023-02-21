fdc885caf71d41919bb9e05155c1f88a8cc64a9c
Yishak <yishakyared12@gmail.com>
2023-02-21 08:39:03 -0800

    added answer

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercse
+ *
+ * Return: void
+ */
+
+int _islower(int c)
+{
+	if (c <= 'z' && c >= a)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
