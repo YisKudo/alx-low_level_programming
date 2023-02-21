aa67bf747074e46dfbc84e721fe1f309a72bea25
Yishak <yishakyared12@gmail.com>
2023-02-21 09:28:08 -0800

    added answer

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_last_digit - checks for lowercse
+ *
+ * @c: the input
+ *
+ * Return: 1 if lowercase or 0 if upper
+ */
+
+int print_last_digit(int c)
+{
+	int lst;
+
+	lst = c % 10;
+	return (lst);
+}
