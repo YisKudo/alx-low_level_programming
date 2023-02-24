a6b0c2f14f3eb285b825f60d047a5e5d709a9c11
Yishak <yishakyared12@gmail.com>
2023-02-24 07:49:11 -0800

    did task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isupper: checks weather char is uppercase
+ * @c: the char to be checked
+ *
+ * return: 1 if C is uppercase and 0 if lower case
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
