6d5b89899d6a340ff5aa7107d7e9465820443573
Yishak <yishakyared12@gmail.com>
2023-02-24 08:12:50 -0800

    fixed betty

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _isupper - checks if parameter is an uppercase character.
+ * _isdigit - checks if the input is a digit.
  * @c: input character.
- * Return: 1 if is an uppercase character, 0 in other case.
+ * Return: 1 if is its a digit, 0 in other case.
  */
 
 int _isdigit(int c)
