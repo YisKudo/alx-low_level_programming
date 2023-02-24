af8fa2b80cd968e6d0c2e01972460c15dfde065a
Yishak <yishakyared12@gmail.com>
2023-02-24 08:00:46 -0800

    fixed betty issues

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * _isupper checks weather char is uppercase
- * @c: the char to be checked
- *
- * Return: 1 if C is uppercase and 0 if lower case
+ * _isupper - checks if parameter is an uppercase character.
+ * @c: input character.
+ * Return: 1 if is an uppercase character, 0 in other case.
  */
 
 int _isupper(int c)
