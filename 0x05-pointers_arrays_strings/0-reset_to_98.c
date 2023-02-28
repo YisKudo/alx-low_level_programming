3815f1010cf466aca43e88f0e1161d7b21c6a0e1
Yishak <yishakyared12@gmail.com>
2023-02-28 08:02:51 -0800

    added initial code

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - update value of pointer to 98
+ *
+ * @*n: input pointer
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	int twnt = 98;
+	
+	n = &twnt;
+	return (n);
+}
