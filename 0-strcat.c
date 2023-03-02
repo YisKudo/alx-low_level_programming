5d1cbd813800bedbfa67dda6164e565105e486cf
yishak <yishakyared12@gmail.com>
2023-03-03 01:27:24 +0300

    fixed

--- 0-strcat.c
+++ 0-strcat.c
@@ -1,28 +1,41 @@
+
 #include "main.h"
-
 /**
- * _strcat - concatenates two strngs
- * @dest: destination
- * @src: source
- * Return: the pointer to dest.
+ * _strcat - concate two string
+ * @dest: input str
+ * @src: input str
+ * Return: pointer to result
  */
-
 char *_strcat(char *dest, char *src)
 {
-	int count = 0, count2 = 0;
+	int num_dest = 0;
+	int num_src = 0;
+	int i = 0, j = 0, y = 0, x;
 
-	while (*(dest + count) != '\0')
+	while (dest[i] != '\0')
 	{
-		count++;
+		i++;
+		num_dest++;
 	}
 
-	while (count2 >= 0)
+	while (src[j] != '\0')
 	{
-		*(dest + count) = *(src + count2);
-		if (*(src + count2) == '\0')
-			break;
-		count++;
-		count2++;
+		j++;
+		num_src++;
 	}
+
+	for (x = num_dest; x <= (num_dest + num_src); x++)
+	{
+		if (x == (num_dest + num_src))
+		{
+			dest[x] = '\0';
+		}
+		else
+		{
+			dest[x] = src[y];
+			y++;
+		}
+	}
+
 	return (dest);
 }
