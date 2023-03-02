34de7bc3ba5688f47ac58b8cb25cdc50398e9d1d
yishak <yishakyared12@gmail.com>
2023-03-03 00:45:37 +0300

    added answer

---
+++ 0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcat -append src to dest
+ *
+ * @dest: char
+ * @src: char
+ * Return: char
+ */
+char *_strcat(char *dest, char *src)
+{
+	int len = 0, i = 0, k;
+
+	while(dest[i] != 0)
+	{
+		len++;
+		i++;
+	}
+	
+	for (k = 0; src[k] != 0; k++)
+	{
+		dest[len] = src[k];
+		len++;
+	}
+}
