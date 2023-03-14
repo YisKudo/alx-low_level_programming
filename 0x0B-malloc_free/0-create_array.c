5a52204e476883da601e813500469938101e4282
Yishak <yishakyared12@gmail.com>
2023-03-14 15:26:53 -0700

    task 0 start

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * *create_array - creates array of chars
+ *
+ * @size: size 
+ * @c: input char
+ * Return: null if size is 0
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *str;
+
+	if (size == 0)
+		return NULL;
+	str = malloc(sizeof(char) * size);
+	str[0] = c;
+}
