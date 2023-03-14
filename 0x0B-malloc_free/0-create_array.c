9c558397210c4438d091eb8fa16b2261437dfcbf
Yishak <yishakyared12@gmail.com>
2023-03-14 15:36:11 -0700

    fixes

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,6 +15,10 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return NULL;
 	str = malloc(sizeof(char) * size);
+	if (str == NULL)
+	{
+		return NULL;
+	}
 	str[0] = c;
 	return (str);
 }
