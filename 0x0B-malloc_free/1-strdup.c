e0a33120ad89820bb0ddc8955a31edc488e92a4e
Yishak <yishakyared12@gmail.com>
2023-03-14 16:04:06 -0700

    corrected

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,9 +12,11 @@ char *_strdup(char *str)
 	char *stp;
 	unsigned int i, j;
 
+	if (str == NULL)
+		return (NULL);
 	for (i = 0; str[i] != '\0'; i++)
 		;
-	stp = malloc(sizeof(char) * i);
+	stp = malloc(sizeof(char) * (i + 1));
 	if (stp == NULL)
 		return (NULL);
 	for (j = 0; j <= i; j++)
