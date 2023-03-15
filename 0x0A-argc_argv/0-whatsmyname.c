0cbe3a6c74395446cbe411560fb9395562e1536e
Yishak <yishakyared12@gmail.com>
2023-03-15 13:16:41 -0700

    added answer

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - main
+ * @argc: length
+ * @argv: names
+ * Return: 0.
+ */
+
+int (int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+	return (0);
+}
