89b322615c8e68e9837dc23b58c95e07fb6447fd
Yishak <yishakyared12@gmail.com>
2023-03-31 08:52:35 -0700

    formatting issue fixes

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -27,7 +27,9 @@ size_t print_list(const list_t *h)
 		if (h -> str == NULL)
 			printf("[0] (nil)");
 		else
-			printf("%s", h -> str);
+			printf("[%u] %s", h -> len, h -> str);
+		
+		printf("\n");
 		h = h -> next;
 		count++;
 	}
