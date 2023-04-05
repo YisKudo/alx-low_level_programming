e7eece461a5c5261f9680ec49124eb12dac77c18
Yishak <yishakyared12@gmail.com>
2023-04-05 09:01:50 -0700

    added

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a list_t list
+ * @head: pointer to the head of the list_t list
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->n);
+		free(temp);
+	}
+}
