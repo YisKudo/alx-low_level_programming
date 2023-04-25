a17842d3139ad2952acaae7fc136736afb90dd43
Yishak <yishakyared12@gmail.com>
2023-04-25 11:38:00 -0700

    done

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file with given content
+ * @filename: the name of the file to create
+ * @text_content: the content to write to the file
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+    int fd, written_bytes;
+    size_t len = 0;
+
+    if (filename == NULL)
+        return (-1);
+
+    fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+    if (fd == -1)
+        return (-1);
+
+    if (text_content != NULL)
+    {
+        while (text_content[len])
+            len++;
+
+        written_bytes = write(fd, text_content, len);
+        if (written_bytes == -1 || written_bytes != len)
+        {
+            close(fd);
+            return (-1);
+        }
+    }
+
+    close(fd);
+    return (1);
+}
+
