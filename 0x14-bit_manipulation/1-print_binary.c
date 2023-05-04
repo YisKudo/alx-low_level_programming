3b4fef6d6481122b66b7b590e193a836e5cd3eb8
yishak <yishakyared12@gmail.com>
2023-05-04 17:11:37 +0300

    used recursv instead

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,33 +1,12 @@
 #include "main.h"
 
 /**
- * print_binary - prints the binary representation of a number
- * @n: the number to be printed in binary
+ * print_binary - Prints the binary representation of a number.
+ * @n: The number to be printed in binary.
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int mask;
-	int flag = 0;
-
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
-
-	mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
-
-	while (mask)
-	{
-		if (n & mask)
-		{
-			flag = 1;
-			_putchar('1');
-		}
-		else if (flag)
-		{
-			_putchar('0');
-		}
-		mask >>= 1;
-	}
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
 }
