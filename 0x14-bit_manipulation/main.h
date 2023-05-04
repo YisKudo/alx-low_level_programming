88f5f82e9d4cb186e8f5dc25dae4825f519ce769
yishak <yishakyared12@gmail.com>
2023-05-04 17:16:23 +0300

    fixed header

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -6,6 +6,7 @@
 unsigned int binary_to_uint(const char *b);
 int _putchar(char c);
 int get_bit(unsigned long int n, unsigned int index);
+void print_binary(unsigned long int n);
 
 #endif /* MAIN_H */
 
