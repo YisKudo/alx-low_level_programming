83a78916cb7f0228e76649776658f09840e7baeb
yishak <yishakyared12@gmail.com>
2023-05-04 17:00:48 +0300

    betty

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,16 +10,23 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int result = 0;
 
-	if (b == NULL) {
+	if (b == NULL)
+	{
 		return (0);
 	}
 
-	while (*b != '\0') {
-		if (*b == '1') {
+	while (*b != '\0')
+	{
+		if (*b == '1')
+		{
 			result = (result << 1) | 1;
-		} else if (*b == '0') {
+		}
+		else if (*b == '0')
+		{
 			result <<= 1;
-		} else {
+		}
+		else
+		{
 			return (0);
 		}
 		b++;
