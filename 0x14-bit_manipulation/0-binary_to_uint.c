4a3e48ded105354be9cf0fc05a1ca7b3343c0dc9
yishak <yishakyared12@gmail.com>
2023-05-04 16:58:05 +0300

    betty

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -4,34 +4,26 @@
  * binary_to_uint - converts a binary to unsigned int
  * @b: input binary
  *
- * Return: the converted number or 0 
+ * Return: the converted number or 0
  */
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int result = 0;
+	unsigned int result = 0;
 
-    if (b == NULL)
-    {
-        return (0);
-    }
+	if (b == NULL) {
+		return (0);
+	}
 
-    while (*b != '\0')
-    {
-        if (*b == '1')
-        {
-            result = (result << 1) | 1;
-        }
-        else if (*b == '0')
-        {
-            result <<= 1;
-        }
-        else
-        {
-            return (0);
-        }
-        b++;
-    }
+	while (*b != '\0') {
+		if (*b == '1') {
+			result = (result << 1) | 1;
+		} else if (*b == '0') {
+			result <<= 1;
+		} else {
+			return (0);
+		}
+		b++;
+	}
 
-    return (result);
+	return (result);
 }
-
