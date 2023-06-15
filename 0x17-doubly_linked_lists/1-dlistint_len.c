3b3660a51947cb4301ba9e2abcb474e8846b0301
Yishak Yared <yishakyared12@gmail.com>
2023-06-15 10:19:49 -0700

    fixed

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -3,16 +3,15 @@
 /**
  * dlistint_len - returns the number of
  * elements in a linked dlistint_t list.
- * @h: first node 
- * Return: number of nodes 
- **/
+ * @h: first node of the linked list
+ * Return: number of nodes
+ */
 size_t dlistint_len(const dlistint_t *h)
 {
 	int count = 0;
 
 	while (h)
-		h = h->next;
-		count++;
+		h = h->next, count++;
 
 	return (count);
 }
